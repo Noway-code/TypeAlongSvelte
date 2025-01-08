0865acdac24a077c975288ede423d2fd07062899
noway-code <dragonfury1928@gmail.com>
2025-01-07 19:26:55 -0500

    Add accuracy window

--- src/routes/book-type/+page.svelte
+++ src/routes/book-type/+page.svelte
@@ -1,292 +1,300 @@
 <script lang="ts"> import { blur } from 'svelte/transition';
-	import '../../styles/type.scss';
-	import { typingWords } from '../../stores/typingStore';
-	import { onMount } from 'svelte';
+import '../../styles/type.scss';
+import { typingWords } from '../../stores/typingStore';
+import { onMount } from 'svelte';
 import { Spring } from 'svelte/motion';
-	/*
-	 Game-specific Types
-	*/
-	type Game = 'waiting for input' | 'in progress' | 'game over';
-	type Word = string;
+/*
+ Game-specific Types
+*/
+type Game = 'waiting for input' | 'in progress' | 'game over';
+type Word = string;
 
-	/*
-	 Subscribe to typingWords
-	*/
-	export let words: Word[] = [];
+/*
+ Subscribe to typingWords
+*/
+export let words: Word[] = [];
 
-	/*
-	 Constants
-	*/
-	const INITIAL_SECONDS = 100;
+/*
+ Constants
+*/
+const INITIAL_SECONDS = 100;
 
-	/*
-	 Game state
-	*/
-	let game: Game = 'waiting for input';
-	let seconds = INITIAL_SECONDS;
-	let typedLetter = '';
+/*
+ Game state
+*/
+let game: Game = 'waiting for input';
+let seconds = INITIAL_SECONDS;
+let typedLetter = '';
 
-	let wordIndex = 0;
-	let letterIndex = 0;
-	let correctLetters = 0;
-	let toggleReset = false;
-	let typedLetters = 0;
-	const stack: number[] = [];
-	const windowSize = 100;
-	let accuracy = new Spring(0, {
-		stiffness: 0.1,
-		damping: 1.6,
-	});
-	let total = 0;
+let wordIndex = 0;
+let letterIndex = 0;
+let correctLetters = 0;
+let toggleReset = false;
+let typedLetters = 0;
+const accStack: number[] = [];
+const windowSize = 100;
+let accuracy = new Spring(0, {
+	stiffness: 0.1,
+	damping: 1.6
+});
+let wpm = new Spring(0, {
+	stiffness: 0.1,
+	damping: 1.6
+});
+let total = 0;
 
-	$: typingWords.subscribe(value => {
-		words = value;
-	});
+const windowDuration = 60000; // 60 seconds window
+type TypedEvent = { time: number; letters: number };
+const wpmStack: TypedEvent[] = [];
 
-	let wordsEl: HTMLDivElement;
-	let letterEl: HTMLSpanElement | null;
-	let inputEl: HTMLInputElement;
-	let caretEl: HTMLDivElement;
 
-	// Accuracy: my idea is a queue where we push up to 250 letters then becomes kind of a sliding window of push and pop
+$: typingWords.subscribe(value => {
+	words = value;
+});
 
-	function accuracyWindow() {
-		if (stack.length > windowSize) {
-			stack.shift();
+let wordsEl: HTMLDivElement;
+let letterEl: HTMLSpanElement | null;
+let inputEl: HTMLInputElement;
+let caretEl: HTMLDivElement;
+
+// Accuracy: my idea is a queue where we push up to 250 letters then becomes kind of a sliding window of push and pop
+
+function accuracyWindow() {
+	if (accStack.length > windowSize) {
+		accStack.shift();
+	}
+	if (typedLetter === words[wordIndex]?.[letterIndex]) {
+		accStack.push(1);
+	} else {
+		accStack.push(0);
+	}
+	total = accStack.reduce((acc, curr) => acc + curr, 0);
+	accuracy.set(parseFloat((total / accStack.length * 100).toFixed(1)));
+
+}
+
+function handleKeydown(event: KeyboardEvent) {
+	const atEndOfWord = letterIndex >= words[wordIndex]?.length;
+
+	if (atEndOfWord) {
+		if (event.code !== 'Space' && event.code !== 'Backspace') {
+			event.preventDefault();
+			return;
 		}
-		if (typedLetter === words[wordIndex]?.[letterIndex]) {
-			stack.push(1);
-		} else {
-			stack.push(0);
-		}
-		total = stack.reduce((acc, curr) => acc + curr, 0);
-		accuracy.set(parseFloat((total / stack.length * 100).toFixed(1)));
-
 	}
 
+	if (event.code === 'Space') {
+		event.preventDefault();
+		if (game === 'in progress') {
+			nextWord();
+		}
+	}
 
-	function handleKeydown(event: KeyboardEvent) {
-		const atEndOfWord = letterIndex >= words[wordIndex]?.length;
+	if (event.code === 'Backspace') {
+		event.preventDefault();
+		if (game !== 'in progress') {
+			return;
+		}
+		handleBackspace();
+	}
 
-		if (atEndOfWord) {
-			if (event.code !== 'Space' && event.code !== 'Backspace') {
-				event.preventDefault();
-				return;
-			}
+	if (game === 'waiting for input') {
+		startGame();
+	}
+
+	focusInput();
+}
+
+function handleBackspace() {
+	// If at the very beginning, do nothing
+	if (wordIndex === 0 && letterIndex === 0) {
+		return;
+	}
+
+	let prevWordIndex = wordIndex;
+	let prevLetterIndex = letterIndex;
+
+	if (letterIndex > 0) {
+		prevLetterIndex -= 1;
+	} else if (wordIndex > 0) {
+		prevWordIndex -= 1;
+		prevLetterIndex = words[prevWordIndex].length - 1;
+	}
+
+	wordIndex = prevWordIndex;
+	letterIndex = prevLetterIndex;
+
+	setLetter();
+
+	if (letterEl) {
+		const previousState = letterEl.dataset.letter;
+
+		// If the previous state was correct, decrement correctLetters
+		if (previousState === 'correct') {
+			correctLetters = Math.max(correctLetters - 1, 0);
 		}
 
-		if (event.code === 'Space') {
-			event.preventDefault();
-			if (game === 'in progress') {
-				nextWord();
-			}
-		}
+		// Reset the data-letter attribute
+		letterEl.dataset.letter = '';
 
-		if (event.code === 'Backspace') {
-			event.preventDefault();
-			if (game !== 'in progress') {
-				return;
-			}
-			handleBackspace();
+		// Temporarily go backwards
+		letterEl = letterEl.previousElementSibling as HTMLSpanElement;
+		if (!letterEl && wordIndex > 0) {
+			letterEl = wordsEl.children[wordIndex - 1].lastElementChild as HTMLSpanElement;
+		} else if (!letterEl && wordIndex === 0) {
+			letterEl = wordsEl.children[wordIndex].firstElementChild as HTMLSpanElement;
 		}
+	}
 
+	moveCaret();
+
+	// Move forward again so the next typed letter overwrites the “empty” space
+	letterEl = letterEl?.nextElementSibling as HTMLSpanElement;
+
+	typedLetters -= 1;
+}
+
+function startGame() {
+	setGameState('in progress');
+	setGameTimer();
+}
+
+function setGameState(state: Game) {
+	game = state;
+}
+
+function setGameTimer() {
+	function gameTimer() {
 		if (game === 'waiting for input') {
-			startGame();
+			clearInterval(interval);
 		}
 
 		focusInput();
 	}
 
-	function handleBackspace() {
-		// If at the very beginning, do nothing
-		if (wordIndex === 0 && letterIndex === 0) {
-			return;
-		}
+	const interval = setInterval(gameTimer, 1000);
+}
 
-		let prevWordIndex = wordIndex;
-		let prevLetterIndex = letterIndex;
-
-		if (letterIndex > 0) {
-			prevLetterIndex -= 1;
-		} else if (wordIndex > 0) {
-			prevWordIndex -= 1;
-			prevLetterIndex = words[prevWordIndex].length - 1;
-		}
-
-		wordIndex = prevWordIndex;
-		letterIndex = prevLetterIndex;
-
-		setLetter();
-
-		if (letterEl) {
-			const previousState = letterEl.dataset.letter;
-
-			// If the previous state was correct, decrement correctLetters
-			if (previousState === 'correct') {
-				correctLetters = Math.max(correctLetters - 1, 0);
-			}
-
-			// Reset the data-letter attribute
-			letterEl.dataset.letter = '';
-
-			// Temporarily go backwards
-			letterEl = letterEl.previousElementSibling as HTMLSpanElement;
-			if (!letterEl && wordIndex > 0) {
-				letterEl = wordsEl.children[wordIndex - 1].lastElementChild as HTMLSpanElement;
-			} else if (!letterEl && wordIndex === 0) {
-				letterEl = wordsEl.children[wordIndex].firstElementChild as HTMLSpanElement;
-			}
-		}
-
-		moveCaret();
-
-		// Move forward again so the next typed letter overwrites the “empty” space
-		letterEl = letterEl?.nextElementSibling as HTMLSpanElement;
-
-		typedLetters -= 1;
+function updateGameState() {
+	if (!wordsEl) {
+		console.warn('wordsEl is not available yet.');
+		return;
 	}
+	setLetter();
+	accuracyWindow();
+	checkLetter();
+	nextLetter();
+	updateLine();
+	resetLetter();
+	moveCaret();
+	debug();
+	focusInput();
+}
 
-	function startGame() {
-		setGameState('in progress');
-		setGameTimer();
+function setLetter() {
+	if (
+		wordsEl &&
+		wordIndex >= 0 &&
+		wordIndex < wordsEl.children.length &&
+		letterIndex >= 0 &&
+		letterIndex < words[wordIndex]?.length
+	) {
+		const currentWord = wordsEl.children[wordIndex] as HTMLElement;
+		letterEl = currentWord.children[letterIndex] as HTMLSpanElement;
+	} else {
+		letterEl = null;
 	}
+}
 
-	function setGameState(state: Game) {
-		game = state;
+
+function checkLetter() {
+	const currentLetter = words[wordIndex]?.[letterIndex];
+	if (!letterEl) return;
+
+	if (typedLetter === currentLetter) {
+		letterEl.dataset.letter = 'correct';
+		increaseScore();
+	} else {
+		letterEl.dataset.letter = 'incorrect';
 	}
+}
 
-	function setGameTimer() {
-		function gameTimer() {
-			if (game === 'waiting for input') {
-				clearInterval(interval);
-			}
+function increaseScore() {
+	correctLetters += 1;
+}
 
-			focusInput();
-		}
+function nextLetter() {
+	letterIndex += 1;
+	typedLetters += 1;
+}
 
-		const interval = setInterval(gameTimer, 1000);
-	}
-
-	function updateGameState() {
-		if (!wordsEl) {
-			console.warn('wordsEl is not available yet.');
-			return;
-		}
-		setLetter();
-		accuracyWindow();
-		checkLetter();
-		nextLetter();
-		updateLine();
-		resetLetter();
-		moveCaret();
-		debug();
-		focusInput();
-	}
-
-	function setLetter() {
-		if (
-			wordsEl &&
-			wordIndex >= 0 &&
-			wordIndex < wordsEl.children.length &&
-			letterIndex >= 0 &&
-			letterIndex < words[wordIndex]?.length
-		) {
-			const currentWord = wordsEl.children[wordIndex] as HTMLElement;
-			letterEl = currentWord.children[letterIndex] as HTMLSpanElement;
-		} else {
-			letterEl = null;
-		}
-	}
-
-
-	function checkLetter() {
-		const currentLetter = words[wordIndex]?.[letterIndex];
-		if (!letterEl) return;
-
-		if (typedLetter === currentLetter) {
-			letterEl.dataset.letter = 'correct';
-			increaseScore();
-		} else {
-			letterEl.dataset.letter = 'incorrect';
-		}
-	}
-
-	function increaseScore() {
-		correctLetters += 1;
-	}
-
-	function nextLetter() {
-		letterIndex += 1;
-		typedLetters += 1;
-	}
-
-	function nextWord() {
-		if (letterIndex !== 0) {
-			const wordRemaining = words[wordIndex].length - letterIndex;
-			typedLetters += wordRemaining;
-			wordIndex += 1;
-			letterIndex = 0;
-			moveCaret();
-		}
-	}
-
-	function updateLine() {
-		const wordEl = wordsEl.children[wordIndex];
-		const wordsY = wordsEl.getBoundingClientRect().y;
-		const wordY = wordEl.getBoundingClientRect().y;
-
-		if (wordY > wordsY) {
-			wordEl.scrollIntoView({ block: 'center' });
-		}
-	}
-
-	function resetLetter() {
-		typedLetter = '';
-	}
-
-	function moveCaret() {
-		const offset = 4;
-		if (!letterEl || !caretEl) return;
-		caretEl.style.top = `${letterEl.offsetTop + offset}px`;
-		caretEl.style.left = `${letterEl.offsetLeft + letterEl.offsetWidth}px`;
-	}
-
-	function resetGame() {
-		toggleReset = !toggleReset;
-
-		setGameState('waiting for input');
-		// When resetting, you might want to re-fetch or re-scrape words from the current page:
-		// await fetchPageWords()  // optionally fetch fresh text from the ebook page
-
-		seconds = INITIAL_SECONDS;
-		typedLetter = '';
-		wordIndex = 0;
+function nextWord() {
+	if (letterIndex !== 0) {
+		const wordRemaining = words[wordIndex].length - letterIndex;
+		typedLetters += wordRemaining;
+		wordIndex += 1;
 		letterIndex = 0;
-		correctLetters = 0;
-		typedLetters = 0;
-		focusInput();
+		moveCaret();
 	}
+}
 
-	function focusInput() {
-		if (inputEl) {
-			inputEl.focus();
-		}
+function updateLine() {
+	const wordEl = wordsEl.children[wordIndex];
+	const wordsY = wordsEl.getBoundingClientRect().y;
+	const wordY = wordEl.getBoundingClientRect().y;
+
+	if (wordY > wordsY) {
+		wordEl.scrollIntoView({ block: 'center' });
 	}
+}
 
-	function debug() {
-		console.log({
-			letterIndex,
-			wordIndex,
-			letterEl,
-			letter: letterEl?.innerText,
-			wordLength: words[wordIndex]?.length
-		});
+function resetLetter() {
+	typedLetter = '';
+}
+
+function moveCaret() {
+	const offset = 4;
+	if (!letterEl || !caretEl) return;
+	caretEl.style.top = `${letterEl.offsetTop + offset}px`;
+	caretEl.style.left = `${letterEl.offsetLeft + letterEl.offsetWidth}px`;
+}
+
+function resetGame() {
+	toggleReset = !toggleReset;
+
+	setGameState('waiting for input');
+	// When resetting, you might want to re-fetch or re-scrape words from the current page:
+	// await fetchPageWords()  // optionally fetch fresh text from the ebook page
+
+	seconds = INITIAL_SECONDS;
+	typedLetter = '';
+	wordIndex = 0;
+	letterIndex = 0;
+	correctLetters = 0;
+	typedLetters = 0;
+	focusInput();
+}
+
+function focusInput() {
+	if (inputEl) {
+		inputEl.focus();
 	}
+}
 
-	onMount(() => {
-		focusInput();
-		accuracy.set(100);
+function debug() {
+	console.log({
+		letterIndex,
+		wordIndex,
+		letterEl,
+		letter: letterEl?.innerText,
+		wordLength: words[wordIndex]?.length
 	});
+}
+
+onMount(() => {
+	focusInput();
+	accuracy.set(100);
+});
 </script>
 
 <!-- PAGE CONTENT -->
@@ -311,8 +319,17 @@ import { Spring } from 'svelte/motion';
 			</svg>
 			Back
 		</a>
-		<div class="accuracy">Accuracy: {accuracy.current.toFixed(1)}</div>
 
+		<div class="stats">
+			<div class="stat">
+				<span>Accuracy</span>
+				<span>{accuracy.current.toFixed(1)}%</span>
+			</div>
+<!--			<div class="stat">-->
+<!--				<span>WPM</span>-->
+<!--				<span>{wpm.current.toFixed(1)}</span>-->
+<!--			</div>-->
+		</div>
 	</div>
 
 	<!-- Game Content -->
